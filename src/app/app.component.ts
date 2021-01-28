efa8724ad7569a3bc000b0fbbde3e8116be4182c
GrayHead <ss.zhuravlov@gmail.com>
2021-01-28 21:16:26 +0200

    init lesson 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,33 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'this is users page';
+  // user = {name: 'kokos', age: 123};
+  //
+  // checkForm(myForm: NgForm): void {
+  //   console.log(myForm);
+  // }
 
+
+  name = new FormControl('', [Validators.required, Validators.minLength(10), this.fukcValidator]);
+  age = new FormControl('', Validators.required);
+  myForm = new FormGroup({
+    name: this.name,
+    age: this.age
+  });
+
+  action(): void {
+    console.log(this.myForm);
+  }
+
+  fukcValidator(inputData: AbstractControl): any {
+    console.log(inputData.value);
+    if (inputData.value.includes('fukc')) {
+      console.log('if');
+      return {fukc: true, msg: 'fukc is presented'};
+    }
+
+    return null;
+
+
+  }
 }
